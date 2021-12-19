3bfe405919337ace76e637d9c2f17401421045a6
imnashu07 <imnashu07@gmail.com>
2021-12-19 18:03:03 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import GetDetails from './components/GetDetails';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [inp , setInp] = useState()
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <span>
+          Name:</span>
+          <input onChange={(e) => {
+            setInp(e.target.value)
+            }}  type="text" name="name" />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+      <img src={`https://source.unsplash.com/400x300/?${inp}`} alt="di"/>
     </div>
   );
 }
---
+++ src/components/GetDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GetDetails() {
+    return (
+        <div>
+            <h1>working data</h1>
+        </div>
+    )
+}
